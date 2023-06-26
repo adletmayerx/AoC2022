d2e2da8cc6f2d1eb6fd0e1e187794656589349ac
adletmayerx <adletmayerx@users.noreply.github.com>
2023-06-26 09:32:03 +0500

    feat: add tsconfig and tested readFileSync

--- day01/solution.ts
+++ day01/solution.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "node:fs";
 
-const lines = readFileSync("day??.txt", { encoding: "utf-8" }) // read day??.txt content
+const lines = readFileSync("./day01/input.txt", { encoding: "utf-8" }) // read day??.txt content
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
   .trim() // Remove starting/ending whitespace
   .split("\n"); // Split on newline
