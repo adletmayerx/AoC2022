32e83c80f9c36921467516fd5fba5bb2ab9b62ed
adletmayerx <adletmayerx@users.noreply.github.com>
2023-06-26 09:44:32 +0500

    feat: solve day01 part1

--- day01/solution.ts
+++ day01/solution.ts
@@ -1,6 +1,14 @@
 import { readFileSync } from "node:fs";
 
-const lines = readFileSync("./day01/input.txt", { encoding: "utf-8" }) // read day??.txt content
+const lines = readFileSync("./day01/input.txt", { encoding: "utf-8" }) // read input.txt content
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
   .trim() // Remove starting/ending whitespace
-  .split("\n"); // Split on newline
+  .split("\n\n"); // Split on newline
+const elfCaloriesSumArray = lines.map((line) =>
+  line.split("\n").reduce((acc, cur) => {
+    return acc + Number(cur);
+  }, 0)
+);
+let maxCalories = -Infinity;
+
+elfCaloriesSumArray.forEach((elf) => (maxCalories = Math.max(maxCalories, elf)));
