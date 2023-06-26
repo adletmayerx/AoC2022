5e8decc4089b32791cdb7e0f77955893f6dd4909
adletmayerx <adletmayerx@users.noreply.github.com>
2023-06-26 08:56:12 +0500

    fc

---
+++ day01/solution.ts
@@ -0,0 +1,6 @@
+import { readFileSync } from "node:fs";
+
+const lines = readFileSync("day??.txt", { encoding: "utf-8" }) // read day??.txt content
+  .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+  .trim() // Remove starting/ending whitespace
+  .split("\n"); // Split on newline
