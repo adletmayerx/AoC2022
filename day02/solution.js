cf2b8da25d3a4fb993bb641f79667ced82f5e71c
adletmayerx <adletmayerx@users.noreply.github.com>
2023-06-27 09:28:55 +0500

    feat: solve day02 part 1

--- day02/solution.js
+++ day02/solution.js
@@ -1,11 +1,20 @@
 import { readFileSync } from "node:fs";
-const lines = readFileSync("./day01/input.txt", { encoding: "utf-8" }) // read input.txt content
+const lines = readFileSync("./day02/input.txt", { encoding: "utf-8" }) // read input.txt content
     .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
     .trim() // Remove starting/ending whitespace
-    .split("\n\n"); // Split on newline
-const elfCaloriesSumArray = lines.map((line) => line.split("\n").reduce((acc, cur) => {
-    return acc + Number(cur);
-}, 0));
-let maxCalories = -Infinity;
-elfCaloriesSumArray.forEach((elf) => (maxCalories = Math.max(maxCalories, elf)));
+    .split("\n"); // Split on newline
+const roundPairArray = lines.map((line) => line.split(" "));
+const OUTCOME_POINTS = {
+    "A X": 4,
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 7,
+    "C Y": 2,
+    "C Z": 6,
+};
+const totalPoints = lines.reduce((acc, cur) => acc + OUTCOME_POINTS[cur], 0);
+console.log(totalPoints);
 //# sourceMappingURL=solution.js.map
--- day02/solution.ts
+++ day02/solution.ts
@@ -1,6 +1,21 @@
 import { readFileSync } from "node:fs";
 
-const lines = readFileSync("./day01/input.txt", { encoding: "utf-8" }) // read input.txt content
+const lines = readFileSync("./day02/input.txt", { encoding: "utf-8" }) // read input.txt content
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
   .trim() // Remove starting/ending whitespace
-  .split("\n\n"); // Split on newline
+  .split("\n"); // Split on newline
+const roundPairArray = lines.map((line) => line.split(" "));
+const OUTCOME_POINTS = {
+  "A X": 4,
+  "A Y": 8,
+  "A Z": 3,
+  "B X": 1,
+  "B Y": 5,
+  "B Z": 9,
+  "C X": 7,
+  "C Y": 2,
+  "C Z": 6,
+} as Record<string, number>;
+
+const totalPoints = lines.reduce((acc, cur) => acc + OUTCOME_POINTS[cur], 0);
+console.log(totalPoints)
