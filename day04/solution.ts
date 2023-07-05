fbd21ed046e0077794307087126d41bb9af5c4f8
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-05 09:35:51 +0500

    feat: solve day04 part1

--- day04/solution.ts
+++ day04/solution.ts
@@ -1,6 +1,29 @@
 import { readFileSync } from "node:fs";
 
-const lines = readFileSync("./day03/input.txt", { encoding: "utf-8" }) // read input.txt content
+const lines = readFileSync("./day04/input.txt", { encoding: "utf-8" }) // read input.txt content
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
   .trim() // Remove starting/ending whitespace
   .split("\n"); // Split on newline
+
+let partOneRes = 0;
+
+lines.forEach((line) => {
+  const elves = line.split(",");
+  const [firstSectionFirst, secondSectionFirst] = elves[0].split("-");
+  const [firstSectionSecond, secondSectionSecond] = elves[1].split("-");
+
+  if (
+    Number(firstSectionFirst) <= Number(firstSectionSecond) &&
+    Number(secondSectionFirst) >= Number(secondSectionSecond)
+  ) {
+    partOneRes++;
+  } else if (
+    Number(firstSectionFirst) >= Number(firstSectionSecond) &&
+    Number(secondSectionFirst) <= Number(secondSectionSecond)
+  ) {
+    partOneRes++;
+  }
+});
+
+console.log(partOneRes);
+
