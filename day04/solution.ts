563f3b15c57b6f1c360b742b16a9e34d3591b1dc
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-05 09:13:45 +0500

    feat: start day04 part1

---
+++ day04/solution.ts
@@ -0,0 +1,6 @@
+import { readFileSync } from "node:fs";
+
+const lines = readFileSync("./day03/input.txt", { encoding: "utf-8" }) // read input.txt content
+  .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+  .trim() // Remove starting/ending whitespace
+  .split("\n"); // Split on newline
